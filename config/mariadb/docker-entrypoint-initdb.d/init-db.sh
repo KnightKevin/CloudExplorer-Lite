8fcc0acb5b7a4717366cd9b8b29503b8a7b03065
Simon <kk@sina.com>
2025-07-22 13:47:11 +0800

    sh

---
+++ config/mariadb/docker-entrypoint-initdb.d/init-db.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# 定义MySQL的用户名和密码
+MYSQL_USER="root"
+MYSQL_PASSWORD="password"
+
+# 定义数据库名称数组
+DATABASES=(
+"zscmp_base"
+"zscmp_connector"
+"zscmp_mc"
+"zscmp_operation"
+"zscmp_oss"
+"zscmp_portal"
+"zscmp_rds"
+"zscmp_ticket"
+"zscmp_vm"
+"zscmp_container"
+"zscmp_zmigrate"
+)
+
+# 创建数据库函数
+create_database() {
+    local db_name=$1
+    mysql -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "CREATE DATABASE IF NOT EXISTS $db_name;"
+    echo "数据库 $db_name 创建完成"
+}
+
+# 循环遍历数据库名称数组，创建数据库
+for db_name in "${DATABASES[@]}"
+do
+    create_database "$db_name"
+done
+
+echo "所有数据库创建完成"
